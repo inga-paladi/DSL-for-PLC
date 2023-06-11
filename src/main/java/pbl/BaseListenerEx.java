b8d21f5e3eaff18802d6fe40dd0d98fd0f54c471
inga-paladi <ingapaladi98@gmail.com>
2023-06-11 12:20:50 +0300

    Update BaseListenerEx.java for the new grammar

--- src/main/java/pbl/BaseListenerEx.java
+++ src/main/java/pbl/BaseListenerEx.java
@@ -1,147 +1,121 @@
 package pbl;
 
 import org.antlr.v4.runtime.misc.NotNull;
-
-import java.util.Optional;
 import java.util.TreeSet;
 
 public class BaseListenerEx extends ProgramBaseListener
 {
-    private enum LogicOperation
-    {
-        Unknown,
-        AND,
-        OR,
-        XOR,
-        NOT
-    }
-
-    TreeSet<ProgramVariable> m_variables;
-    ProgramVariable.Type m_currentType;
+    TreeSet<String> m_variables;
 
     public BaseListenerEx()
     {
-        m_variables = new TreeSet<ProgramVariable>();
-        m_currentType = ProgramVariable.Type.Unknown;
+        m_variables = new TreeSet<String>();
     }
 
+    // Variables redeclaration check -----------------------------------------------------------------------------------
     @Override
-    public void enterInputDeclarations(ProgramParser.InputDeclarationsContext ctx)
+    public void enterInputVarDeclaration(@NotNull ProgramParser.InputVarDeclarationContext ctx)
     {
-        m_currentType = ProgramVariable.Type.Input;
-    }
-
-    @Override
-    public void exitInputDeclarations(ProgramParser.InputDeclarationsContext ctx)
-    {
-        m_currentType = ProgramVariable.Type.Unknown;
-    }
-
-    @Override
-    public void enterOutputDeclarations(ProgramParser.OutputDeclarationsContext ctx)
-    {
-        m_currentType = ProgramVariable.Type.Output;
-    }
-
-    @Override
-    public void exitOutputDeclarations(ProgramParser.OutputDeclarationsContext ctx)
-    {
-        m_currentType = ProgramVariable.Type.Unknown;
-    }
-
-    @Override
-    public void enterVar(ProgramParser.VarContext ctx)
-    {
-        if (m_currentType == ProgramVariable.Type.Unknown)
-            return;
-
-        String varName = ctx.getText();
-        if (GetVariable(varName).isPresent())
+        String inputVarName = ctx.InputVar().getText();
+        if (m_variables.contains(inputVarName))
         {
-            System.out.println("Variable redeclaration: " + varName);
-            System.exit(0);
+            System.out.println("Input variable redeclaration: " + inputVarName);
+            Exit();
         }
 
-        m_variables.add(new ProgramVariable(ctx.getText(), m_currentType, false));
+        m_variables.add(inputVarName);
     }
 
-    @Override public void enterAssignment(@NotNull ProgramParser.AssignmentContext ctx)
+    @Override
+    public void enterOutputVarDeclaration(@NotNull ProgramParser.OutputVarDeclarationContext ctx)
     {
-        String lVarName = ctx.var().getText();
-        Optional<ProgramVariable> lVar = GetVariable(lVarName);
-        if (!lVar.isPresent())
+        String outputVarName = ctx.OutputVar().getText();
+        if (m_variables.contains(outputVarName))
         {
-            VarNotDeclared(ctx.getText(), lVarName);
-            System.exit(0);
+            System.out.println("Output variable redeclaration: " + outputVarName);
+            Exit();
         }
 
-        if (ctx.expression().constant() != null)
+        m_variables.add(outputVarName);
+    }
+
+    @Override
+    public void enterMemoryDeclaration(@NotNull ProgramParser.MemoryDeclarationContext ctx)
+    {
+        String memoryName = ctx.MemVar().getText();
+        if (m_variables.contains(memoryName))
         {
-            String constant = ctx.expression().constant().getText().substring(1);
-            boolean constValue = ctx.expression().constant().getText().substring(1).equals("1");
-            lVar.get().SetValue(constValue);
+            System.out.println("Memory variable redeclaration: " + memoryName);
+            Exit();
         }
-        else if (ctx.expression().var() != null)
+
+        m_variables.add(memoryName);
+    }
+
+    // Check usage of undeclared variables -----------------------------------------------------------------------------
+    @Override
+    public void enterAssignment(@NotNull ProgramParser.AssignmentContext ctx)
+    {
+        // Check left value
+        String lValName = ctx.lValue().getText();
+        if (!m_variables.contains(lValName))
         {
-            String rVarName = ctx.expression().var().getText();
-            Optional<ProgramVariable> rVar = GetVariable(rVarName);
-            if (!rVar.isPresent())
+            VarNotDeclared(ctx.getText(), lValName);
+            Exit();
+        }
+
+        // Check right value
+
+        // If rValue is input variable
+        if (ctx.rValue().InputVar() != null)
+        {
+            String rValName = ctx.rValue().InputVar().getText();
+            if (!m_variables.contains(rValName))
             {
-                VarNotDeclared(ctx.getText(), rVarName);
-                System.exit(0);
+                VarNotDeclared(ctx.getText(), rValName);
+                Exit();
+            }
+        }
+
+        // If rValue is memory variable
+        if (ctx.rValue().MemVar() != null)
+        {
+            String rValName = ctx.rValue().MemVar().getText();
+            if (!m_variables.contains(rValName))
+            {
+                VarNotDeclared(ctx.getText(), rValName);
+                Exit();
+            }
+        }
+    }
+
+    @Override
+    public void enterOperation(@NotNull ProgramParser.OperationContext ctx)
+    {
+        // Check operands
+        for (ProgramParser.OperandContext operand: ctx.operand()) {
+            // If operand is input var
+            if (operand.InputVar() != null)
+            {
+                String rValName = operand.InputVar().getText();
+                if (!m_variables.contains(rValName))
+                {
+                    VarNotDeclared(ctx.getText(), rValName);
+                    Exit();
+                }
             }
 
-            lVar.get().SetValue(rVar.get().GetValue());
+            // If operand is memory var
+            if (operand.MemVar() != null)
+            {
+                String rValName = operand.MemVar().getText();
+                if (!m_variables.contains(rValName))
+                {
+                    VarNotDeclared(ctx.getText(), rValName);
+                    Exit();
+                }
+            }
         }
-    }
-
-    @Override
-    public void enterStatement(@NotNull ProgramParser.StatementContext ctx)
-    {
-        String lVarName = ctx.var(0).getText();
-        String rVarName = ctx.var(1).getText();
-
-        Optional<ProgramVariable> lVar = GetVariable(lVarName);
-        Optional<ProgramVariable> rVar = GetVariable(rVarName);
-        if (!lVar.isPresent())
-        {
-            VarNotDeclared(ctx.getText(), lVarName);
-            System.exit(0);
-        }
-        else if (!rVar.isPresent())
-        {
-            VarNotDeclared(ctx.getText(), rVarName);
-            System.exit(0);
-        }
-
-        ExecuteLogicOperation(lVar.get(), ctx.logicOp().getText(), rVar.get());
-    }
-
-    private Optional<ProgramVariable> GetVariable(String name)
-    {
-        // Find the object based on a specific value
-        return m_variables.stream().filter(variable -> variable.GetName().equals(name)).findFirst();
-    }
-
-    private void ExecuteLogicOperation(ProgramVariable lVar, String logicOperation, ProgramVariable rVar)
-    {
-        boolean result = false;
-        if (logicOperation.equals("AND"))
-            result = lVar.GetValue() && rVar.GetValue();
-        else if (logicOperation.equals("OR"))
-            result = lVar.GetValue() || rVar.GetValue();
-        else if (logicOperation.equals("XOR"))
-            result = lVar.GetValue() ^ rVar.GetValue();
-        else if (logicOperation.equals("NOT"))
-            result = !rVar.GetValue();
-
-        lVar.SetValue(result);
-    }
-
-    public void exitProgram(@NotNull ProgramParser.ProgramContext ctx)
-    {
-        for (ProgramVariable variable : m_variables)
-            System.out.println(variable.GetName() + " = " + String.valueOf(variable.GetValue()));
     }
 
     private void VarNotDeclared(String statement, String varName)
@@ -149,4 +123,5 @@ public class BaseListenerEx extends ProgramBaseListener
         System.out.println(varName + " not declared in statement: " + statement);
     }
 
+    private void Exit() { System.exit(0); }
 }
